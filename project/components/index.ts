6fa842f40cc3023a8260cd7a63d7f56990f051a9
NonsoBarn <102404755+NonsoBarn@users.noreply.github.com>
2023-06-18 21:52:23 +0100

    initial commit

---
+++ project/components/index.ts
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CarCard from "./CarCard";
+import SearchManufacturer from "./SearchManufacturer";
+import CardDetails from "./CardDetails";
+
+export {
+  Navbar,
+  Footer,
+  Hero,
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  SearchBar,
+  SearchManufacturer,
+  CardDetails,
+};
